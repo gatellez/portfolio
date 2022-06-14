96f1ded69541c8adde9d1eb5ff70be9c7931819b
Gilber Tellez <gatelleza@unal.edu.co>
2022-06-14 17:45:54 -0500

    Segundo commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
+from asyncio.windows_events import NULL
+from email.mime import image
+from email.policy import default
+from turtle import title
 from django.db import models
+from django.db.models.fields import CharField
+from django.db.models.fields.files import ImageField
+from django.db.models.fields import URLField
 
-# Create your models here.
+class Project(models.Model):
+    titulo = CharField(max_length=100, default="Titulo")
+    descripcion = CharField(max_length=250, default="Descripcion")
+    imagen = ImageField(default=NULL)
+    url=URLField(blank=True)
